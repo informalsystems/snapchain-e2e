af87d77e77f194f6a83b1e97055b4bc1546f1179
Bastien Faivre <faivre.bast@gmail.com>
2025-05-22 09:27:45 +0200

    fix: scripts

--- e2e/scripts/snapshot.sh
+++ e2e/scripts/snapshot.sh
@@ -7,5 +7,5 @@ mkdir -p logs
 
 for id in "$@"; do
   echo "Saving logs for node $id"
-  docker logs "snapchain-e2e-node$id-1" > "logs/node$id.log"
+  docker compose logs "node$id" > "logs/node$id.log"
 done
